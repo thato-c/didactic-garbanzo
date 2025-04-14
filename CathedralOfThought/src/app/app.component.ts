1d0f9037069ebe1ebba9b56ae55128fdcf63be1f
Thato <t.chitja369@gmail.com>
2025-04-14 15:28:36 +0200

    Edit Footer Component

--- CathedralOfThought/src/app/app.component.ts
+++ CathedralOfThought/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent, ProjectsComponent, NavigationComponent],
+  imports: [RouterOutlet, HomeComponent, ProjectsComponent, NavigationComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   standalone: true,

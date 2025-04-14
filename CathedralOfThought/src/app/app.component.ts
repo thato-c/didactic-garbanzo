8c846050fea25c4460e19b701e9ce7f5b1c9edc3
Thato <t.chitja369@gmail.com>
2025-04-14 13:42:21 +0200

    Add Project Page and Design the Interface

--- CathedralOfThought/src/app/app.component.ts
+++ CathedralOfThought/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, ProjectsComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  standalone: true,
 })
 export class AppComponent {
   title = 'CathedralOfThought';

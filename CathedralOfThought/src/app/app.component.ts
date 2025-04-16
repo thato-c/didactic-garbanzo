af389c3a5b889150e81b2f5444e501ec77fb26f0
Thato <t.chitja369@gmail.com>
2025-04-16 11:51:23 +0200

    Update app-root styles

--- CathedralOfThought/src/app/app.component.ts
+++ CathedralOfThought/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent, ProjectsComponent, NavigationComponent, FooterComponent],
+  imports: [RouterOutlet, NavigationComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   standalone: true,
